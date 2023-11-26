156fe3164586273752aa1d33d28e9b65f53a3dee
vyahislav <khv-rvv@mail.ru>
2023-11-26 13:19:15 +1000

    first commit

---
+++ Задание_3.sql
@@ -0,0 +1,30 @@
+-- Количество исполнителей в каждом жанре
+SELECT g.name, p.artist_name, COUNT(artist_name) FROM genre g
+JOIN performer_genre p_g ON g.genre_id = p_g.genre_id
+JOIN performer p ON p_g.performer_id = p.performer_id
+GROUP BY g.name, p.artist_name
+ORDER BY 2;
+
+--Количество треков, вошедших в альбомы 2019–2020 годов.
+SELECT release_year, COUNT(album_name) AS number_of_albums
+FROM album
+WHERE release_year BETWEEN 1994 AND 2002
+GROUP BY release_year
+ORDER BY release_year;
+
+--Средняя продолжительность треков по каждому альбому.
+SELECT AVG(t.duration), a.album_name n FROM track t
+JOIN album a ON a.album_id = t.album_id
+GROUP BY n;
+
+-- все исполнители, которые не выпустили альбомы в 2020 году.
+SELECT p.artist_name, a1.release_year FROM public.performer p
+JOIN artist_album a2 ON a2.performer_id = p.performer_id
+JOIN album a1 ON a1.album_id = a1.album_id
+WHERE a1.release_year !=2020
+GROUP BY p.artist_name, a1.release_year;
+
+-- Названия сборников, в которых присутствует
+-- конкретный исполнитель (выберите его сами).
+SELECT c.collection_name FROM collection c
+WHERE c.collection_name LIKE '%Elton John%'
